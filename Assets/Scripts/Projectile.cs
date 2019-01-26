6652c813aa80a8edaa388a643a17b5dc64885cb6
Ben <benbrinkman@L-CB4111-27>
2019-01-25 21:20:51 -0500

    added folders and projectiles

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+
+    public float maxLifeTime;
+    private float currentLife;
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+        currentLife = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentLife += Time.deltaTime;
+        transform.position += new Vector3(transform.position.x, transform.position.y, speed*Time.deltaTime);
+        if (currentLife >= maxLifeTime)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
