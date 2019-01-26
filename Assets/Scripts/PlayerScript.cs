acd87654c05dd3994da579c2a8fc6a8def7bbb9b
Magneseus <matt.bmayer@gmail.com>
2019-01-25 23:02:39 -0500

    Pushing default Rewired config.
    Copy the "Rewired Input Manager" object to your scene. Look at the PlayerScript for reference of how to use the input manager in a class.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
+    public int playerId = 0;
+
+    private Rewired.Player rePlayer;
+    private CharacterController cc;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        rePlayer = ReInput.players.GetPlayer(playerId);
+        cc = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        DoInput();
 	}
+
+    private void DoInput()
+    {
+        Vector3 moveVec = new Vector3();
+        moveVec.x = rePlayer.GetAxis("LStick Horizontal");
+        moveVec.z = rePlayer.GetAxis("LStick Vertical");
+
+
+        cc.Move(moveVec * 3.0f * Time.deltaTime);
+    }
 }
