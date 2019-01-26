975a43ffd0d314c67ac17fb6dfaa68ae91acad50
Ben <benbrinkman@L-CB4111-27>
2019-01-26 02:33:46 -0500

    menus

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+    
+    private Rewired.Player rePlayer;
+
+    // Use this for initialization
+    void Start () {
+
+        rePlayer = ReInput.players.GetPlayer(0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+        if (rePlayer.GetButton("A Button"))
+        {
+            SceneManager.LoadScene("CharacterSelect");
+        }
+        
+	}
+    
+
+}
