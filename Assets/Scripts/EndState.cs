af4fd7ba419cf4da6e1b844d34f131aa14a39d09
Josh <35538442+joshvdl@users.noreply.github.com>
2019-01-27 14:27:41 -0500

    doors end and stuff

--- Assets/Scripts/EndState.cs
+++ Assets/Scripts/EndState.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndState : MonoBehaviour {
 
+    public Text end;
 	// Use this for initialization
 	void Start () {
 		
@@ -20,6 +22,7 @@ public class EndState : MonoBehaviour {
         if (collision.gameObject.tag == "Player")
         {
             //gameover
+            end.gameObject.SetActive(true);
         }
     }
 }
