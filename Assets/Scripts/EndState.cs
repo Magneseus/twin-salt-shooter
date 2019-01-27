44ee689054aa3dc74589762f347326a4df7f0646
Ben <benbrinkman@L-CB4111-27>
2019-01-27 12:18:43 -0500

    l

---
+++ Assets/Scripts/EndState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndState : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (this.gameObject.tag == "Player")
+        {
+
+        }
+    }
+}
